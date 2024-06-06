7b7aaa90546134bdc00a5431be68288fb16fcd19
jack.gronenthal <jack.gronenthal@servicenow.com>
2024-06-05 17:31:54 -0700

    Reducing parameters into argument object

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const providerType = core.getInput('provider');
 
-
-    const arguments = { providerType };
+    const parameters = [ 'args', 'provider', 'repository', 'path' ];
+    const arguments = parameters.reduce((acc, arg) => ({ ...acc, [arg]: core.getInput(arg) }), {});
 
     console.log(`Arguments ${JSON.stringify(arguments, null, 2)}`);
     core.setOutput("time", "ABC");
